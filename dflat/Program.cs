75b0dd0d349ac959f34dc3b840d95a280cbbb8fe
SimonFJ20 <simonfromjakobsen@gmail.com>
2023-01-23 11:55:23 +0100

    split into files

--- dflat/Program.cs
+++ dflat/Program.cs
@@ -1,84 +1,8 @@
 ﻿using System;
 
-namespace dflat
-{
-    enum TokenType
-    {
-        Eof,
-        Error,
-
-        Id,
-        Int,
-        Float,
-        Char,
-        String,
-
-        Null,
-        False,
-        True,
-        If,
-        Else,
-        While,
-        For,
-        Break,
-        Continue,
-        Fn,
-        Return,
-        Class,
-        New,
-        Pub,
-        Not,
-        And,
-        Or,
-        In,
-        
-        LParen,
-        RParen,
-        LBrace,
-        RBrace,
-        LBracket,
-        RBracket,
-
-        DoubleEqual,
-        Equal,
-        PlusEqual,
-        MinusEqual,
-        AsteriskEqual,
-        SlashEqual,
-        PercentEqual,
-        Plus,
-        LtEqual,
-        Lt,
-        GtEqual,
-        Gt,
-        ExclamationEqual,
-    }
-
-    struct Token
-    {
-        TokenType type;
-        String value;
-        int line, column;
-    }
-
-    interface Lexer
-    {
-        Token next();
-    }
-
-    class Parser
-    {
-        private Lexer lexer;
-
-        Parser(Lexer lexer) {
-            this.lexer = lexer;
-        }
-    }
-
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+namespace dflat {
+    internal class Program {
+        static void Main(string[] args) {
             Console.WriteLine("Hello World!");
         }
     }
